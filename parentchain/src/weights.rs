31cf747a0240a319b141580eabadb52794d3e509
echevrier <84318241+echevrier@users.noreply.github.com>
2021-11-29 16:24:31 +0100

    Add parentchain-pallet (#22)
    * Add a pallet to track parentchain specific values? (#12)
    
    * Add sidechain pallet parentchain
    
    * Add sidechain pallet parentchain
    
    Co-authored-by: Gaudenz Kessler <gaudenz.kessler@scs.ch>
    
    * Update parentchain pallet to new substrate
    
    * Changes from review: don't have any any benchmarks
    
    * Remove non needed frame-benchmarking from cargo in parentchain pallet
    
    * Remove non needed hex-literal from cargo in parentchain pallet
    
    Co-authored-by: gaudenzkessler <92718752+gaudenzkessler@users.noreply.github.com>
    Co-authored-by: Gaudenz Kessler <gaudenz.kessler@scs.ch>
    Co-authored-by: echevrier <edith.chevrier@scs.ch>

---
+++ parentchain/src/weights.rs
@@ -0,0 +1,13 @@
+pub use frame_support::weights::{constants::RocksDbWeight, Weight};
+
+/// Weight functions needed for pallet_parentchain.
+pub trait WeightInfo {
+	fn set_block() -> Weight;
+}
+
+/// Weights for pallet_parentchain using the Integritee parachain node and recommended hardware.
+impl WeightInfo for () {
+	fn set_block() -> Weight {
+		10_000 as Weight
+	}
+}
