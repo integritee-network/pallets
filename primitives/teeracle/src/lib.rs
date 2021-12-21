b35ba483083e9347014de536f3ed441e607269b7
echevrier <edith.chevrier@scs.ch>
2021-12-21 17:06:37 +0100

    Remove unused

--- primitives/teeracle/src/lib.rs
+++ primitives/teeracle/src/lib.rs
@@ -1,7 +1,6 @@
 //!Primitives for teeracle
 #![cfg_attr(not(feature = "std"), no_std)]
 use common_primitives::PalletString;
-use sp_std::prelude::*;
 use substrate_fixed::types::U32F32;
 
 pub type ExchangeRate = U32F32;
