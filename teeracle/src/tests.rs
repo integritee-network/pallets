beffcf1c5494ba7fbd2b8705a917b800e38f6e95
orion <szilard.parrag@gmail.com>
2022-09-28 16:35:54 +0200

    clippy: bool_assert_comparison

--- teeracle/src/tests.rs
+++ teeracle/src/tests.rs
@@ -101,13 +101,10 @@ fn get_existing_exchange_rate_works() {
 #[test]
 fn get_inexisting_exchange_rate_is_zero() {
 	new_test_ext().execute_with(|| {
-		assert_eq!(
-			ExchangeRates::<Test>::contains_key(
-				DOT_USD_TRADING_PAIR.to_owned(),
-				COINGECKO_SRC.to_owned()
-			),
-			false
-		);
+		assert!(!ExchangeRates::<Test>::contains_key(
+			DOT_USD_TRADING_PAIR.to_owned(),
+			COINGECKO_SRC.to_owned()
+		));
 		assert_eq!(
 			Exchange::exchange_rate(DOT_USD_TRADING_PAIR.to_owned(), COINGECKO_SRC.to_owned()),
 			U32F32::from_num(0)
@@ -129,13 +126,10 @@ fn update_exchange_rate_to_none_delete_exchange_rate() {
 			DOT_USD_TRADING_PAIR.to_owned(),
 		));
 		assert!(System::events().iter().any(|a| a.event == expected_event));
-		assert_eq!(
-			ExchangeRates::<Test>::contains_key(
-				DOT_USD_TRADING_PAIR.to_owned(),
-				COINGECKO_SRC.to_owned()
-			),
-			false
-		);
+		assert!(!ExchangeRates::<Test>::contains_key(
+			DOT_USD_TRADING_PAIR.to_owned(),
+			COINGECKO_SRC.to_owned()
+		));
 	})
 }
 
@@ -154,13 +148,10 @@ fn update_exchange_rate_to_zero_delete_exchange_rate() {
 		));
 
 		assert!(System::events().iter().any(|a| a.event == expected_event));
-		assert_eq!(
-			ExchangeRates::<Test>::contains_key(
-				DOT_USD_TRADING_PAIR.to_owned(),
-				COINGECKO_SRC.to_owned()
-			),
-			false
-		);
+		assert!(!ExchangeRates::<Test>::contains_key(
+			DOT_USD_TRADING_PAIR.to_owned(),
+			COINGECKO_SRC.to_owned()
+		));
 	})
 }
 
