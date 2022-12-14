b39601a921889f45db03087805fc77e7091c4bd4
Andrew Burger <andrewburger1991@gmail.com>
2022-12-14 10:28:14 +0100

    Clippy and update substrate and polkadot versions

--- primitives/xcm-transactor/src/lib.rs
+++ primitives/xcm-transactor/src/lib.rs
@@ -1,59 +1,56 @@
 #![cfg_attr(not(feature = "std"), no_std)]
-use xcm::{latest::Weight as XcmWeight, prelude::*};
-use codec::{FullCodec, Encode, Decode};
-use frame_support::RuntimeDebug;
+use codec::{Decode, Encode, FullCodec};
 pub use cumulus_primitives_core::ParaId;
+use frame_support::RuntimeDebug;
+use xcm::{latest::Weight as XcmWeight, prelude::*};
 
 pub trait BuildRelayCall {
-    type RelayCall: FullCodec;
-    /// TODO: Add Notes about which function this is and why it is being used
-    ///
-    ///
-    fn swap_call(self_para_id: ParaId, other_para_id: ParaId) -> Self::RelayCall;
+	type RelayCall: FullCodec;
+	/// TODO: Add Notes about which function this is and why it is being used
+	///
+	///
+	fn swap_call(self_para_id: ParaId, other_para_id: ParaId) -> Self::RelayCall;
 
-    /// Wraps constructed Relaychain Call in an XCM message to be dispatched via 'send_xcm'
-    ///
-    /// Params:
-    /// - RelayCall (Different depending on Kusama or Polkadot)
-    /// - execution to be purchased via BuyExecution Xcm Instruction
-    /// - Weight required to execute this call.
-    ///
-    /// Returns:
-    /// - Corresponding Xcm Message for Transacting on this RelayCall
-    ///
-    fn construct_transact_xcm(
-        call: Self::RelayCall,
-        weight: XcmWeight
-    ) -> Xcm<()>;
+	/// Wraps constructed Relaychain Call in an XCM message to be dispatched via 'send_xcm'
+	///
+	/// Params:
+	/// - RelayCall (Different depending on Kusama or Polkadot)
+	/// - execution to be purchased via BuyExecution Xcm Instruction
+	/// - Weight required to execute this call.
+	///
+	/// Returns:
+	/// - Corresponding Xcm Message for Transacting on this RelayCall
+	///
+	fn construct_transact_xcm(call: Self::RelayCall, weight: XcmWeight) -> Xcm<()>;
 }
 
 #[derive(Encode, Decode, RuntimeDebug)]
 pub enum RegistrarCall {
-    /// Corresponds to the swap extrinsic index within the Registrar Pallet
-    #[codec(index = 3)]
-    Swap(ParaId, ParaId)
+	/// Corresponds to the swap extrinsic index within the Registrar Pallet
+	#[codec(index = 3)]
+	Swap(ParaId, ParaId),
 }
 
 #[cfg(feature = "kusama")]
 pub mod kusama {
-    use crate::*;
-    #[derive(Encode, Decode, RuntimeDebug)]
-    pub enum RelayRuntimeCall {
-        /// Corresponds to the pallet index within the Kusama Runtime
-        #[codec(index = 70)]
-        Registrar(RegistrarCall)
-    }
+	use crate::*;
+	#[derive(Encode, Decode, RuntimeDebug)]
+	pub enum RelayRuntimeCall {
+		/// Corresponds to the pallet index within the Kusama Runtime
+		#[codec(index = 70)]
+		Registrar(RegistrarCall),
+	}
 }
 
 #[cfg(feature = "polkadot")]
 pub mod polkadot {
-    use crate::*;
-    #[derive(Encode, Decode, RuntimeDebug)]
-    pub enum RelayRuntimeCall {
-        /// Corresponds to the pallet index within the Polkadot Runtime
-        #[codec(index = 70)]
-        Registrar(RegistrarCall)
-    }
+	use crate::*;
+	#[derive(Encode, Decode, RuntimeDebug)]
+	pub enum RelayRuntimeCall {
+		/// Corresponds to the pallet index within the Polkadot Runtime
+		#[codec(index = 70)]
+		Registrar(RegistrarCall),
+	}
 }
 
 #[cfg(feature = "kusama")]
@@ -64,22 +61,17 @@ pub use polkadot::*;
 
 pub struct RelayCallBuilderType;
 impl BuildRelayCall for RelayCallBuilderType {
-    type RelayCall = RelayRuntimeCall;
+	type RelayCall = RelayRuntimeCall;
 
-    fn swap_call(self_para_id: ParaId, other_para_id: ParaId) -> Self::RelayCall {
-        Self::RelayCall::Registrar(RegistrarCall::Swap(self_para_id, other_para_id))
-    }
+	fn swap_call(self_para_id: ParaId, other_para_id: ParaId) -> Self::RelayCall {
+		Self::RelayCall::Registrar(RegistrarCall::Swap(self_para_id, other_para_id))
+	}
 
-    fn construct_transact_xcm(
-        call: Self::RelayCall,
-        weight: XcmWeight
-    ) -> Xcm<()> {
-        Xcm(vec![
-            Transact {
-                origin_type: OriginKind::Native,
-                require_weight_at_most: weight,
-                call: call.encode().into(),
-            },
-        ])
-    }
+	fn construct_transact_xcm(call: Self::RelayCall, weight: XcmWeight) -> Xcm<()> {
+		Xcm(vec![Transact {
+			origin_type: OriginKind::Native,
+			require_weight_at_most: weight,
+			call: call.encode().into(),
+		}])
+	}
 }
--- xcm-transactor/src/lib.rs
+++ xcm-transactor/src/lib.rs
@@ -4,48 +4,48 @@ pub use pallet::*;
 
 #[frame_support::pallet]
 pub mod pallet {
-    use frame_support::{pallet_prelude::*, weights::constants::WEIGHT_PER_SECOND};
-    use frame_system::pallet_prelude::*;
-    use cumulus_primitives_core::ParaId;
-    use xcm::{latest::prelude::*, VersionedMultiLocation};
-    use xcm_transactor_primitives::*;
+	use cumulus_primitives_core::ParaId;
+	use frame_support::pallet_prelude::*;
+	use frame_system::pallet_prelude::*;
+	use xcm::latest::prelude::*;
+	use xcm_transactor_primitives::*;
 
-    #[pallet::pallet]
-    #[pallet::generate_store(pub(super) trait Store)]
-    #[pallet::without_storage_info]
-    pub struct Pallet<T>(PhantomData<T>);
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	#[pallet::without_storage_info]
+	pub struct Pallet<T>(PhantomData<T>);
 
-    #[pallet::config]
-    pub trait Config: frame_system::Config {
-        type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
 
-        type RelayCallBuilder: BuildRelayCall;
+		type RelayCallBuilder: BuildRelayCall;
 
-        type XcmSender: SendXcm;
+		type XcmSender: SendXcm;
 
-        #[pallet::constant]
-        type ShellRuntimeParaId: Get<ParaId>;
+		#[pallet::constant]
+		type ShellRuntimeParaId: Get<ParaId>;
 
-        #[pallet::constant]
-        type IntegriteeKsmParaId: Get<ParaId>;
-    }
+		#[pallet::constant]
+		type IntegriteeKsmParaId: Get<ParaId>;
+	}
 
-    #[pallet::event]
-    pub enum Event<T: Config> {
-        TransactSent,
-    }
+	#[pallet::event]
+	pub enum Event<T: Config> {
+		TransactSent,
+	}
 
-    #[pallet::error]
-    pub enum Error<T> {
-        TransactFailed,
-    }
+	#[pallet::error]
+	pub enum Error<T> {
+		TransactFailed,
+	}
 
-    #[pallet::call]
-    impl<T: Config> Pallet<T> {
-        #[pallet::weight(0)]
-        pub fn send_swap_ump(origin: OriginFor<T>) -> DispatchResult {
-            ensure_root(origin)?;
-            Ok(())
-        }
-    }
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		#[pallet::weight(0)]
+		pub fn send_swap_ump(origin: OriginFor<T>) -> DispatchResult {
+			ensure_root(origin)?;
+			Ok(())
+		}
+	}
 }
