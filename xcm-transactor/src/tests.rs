dba4badec2374549482c2c0cf42ea06849f4cb20
Andrew Burger <andrewburger1991@gmail.com>
2022-12-19 15:26:24 +0100

    Trying to add subxt to import kusama runtime types

--- xcm-transactor/src/tests.rs
+++ xcm-transactor/src/tests.rs
@@ -3,6 +3,17 @@ use cumulus_primitives_core::ParaId;
 use sp_keyring::AccountKeyring;
 use frame_support::{assert_err, assert_noop, assert_ok};
 
+#[subxt::subxt(runtime_metadata_url = "wss://kusama-rpc.polkadot.io:443")]
+pub mod kusama {}
+
+use kusama::runtime_types::{
+    kusama_runtime::RuntimeCall,
+    pallet_proxy::pallet::Call as ProxyCall,
+    pallet_utility::pallet::Call as UtilityCall,
+    pallet_society::pallet::Call as SocietyCall,
+    // paras_registrar::pallet::Call as RegistrarCall,
+};
+
 #[test]
 fn swap_ump_fails_not_privileged() {
     new_test_ext().execute_with(|| {
@@ -32,5 +43,18 @@ fn swap_ump_fails_1_id_invalid() {
             XcmTransactor::send_swap_ump(RuntimeOrigin::root(), shell_id.into(), ParaId::from(20000)),
             Error::<Test>::InvalidSwapIds
         );
+    })
+}
+
+#[test]
+fn swap_ump_success() {
+    new_test_ext().execute_with(|| {
+        let shell_id = <Test as Config>::ShellRuntimeParaId::get();
+        let integritee_id = <Test as Config>::IntegriteeKsmParaId::get();
+        assert_ok!(XcmTransactor::send_swap_ump(RuntimeOrigin::root(), shell_id.into(), integritee_id.into()));
+        assert!(System::events().iter().any(|swap| matches!(
+            swap.event,
+            RuntimeEvent::XcmTransactor(crate::Event::TransactSent { .. })
+        )));
     })
 }
