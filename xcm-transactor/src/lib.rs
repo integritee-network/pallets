72cc118af8f11b9d4d4bcfbb53ea0eb0d618b5e8
Christian Langenbacher <clangenb+gh@protonmail.ch>
2023-01-11 09:45:20 +0100

    typo

--- xcm-transactor/src/lib.rs
+++ xcm-transactor/src/lib.rs
@@ -84,7 +84,7 @@ pub mod pallet {
 		///  Rococo-Local as of 11.01.2022:
 		///		* xcm_weight: 10_000_000_000
 		///		* buy_execution_weight: 500_000_000
-		///  Kusama: do be defined, but the weights will be higher than on Rococo-Local
+		///  Kusama: to be defined, but the weights will be higher than on Rococo-Local
 		///
 		#[pallet::call_index(0)]
 		#[pallet::weight(46_200_000)] // Arbitrary weight.
