d0d86e33a7812c54030bdf8909e9870ff920a9df
Szilard Parrag <szilard.parrag@gmail.com>
2023-05-26 09:22:21 +0200

    remove deprecated trait Store uses
    See https://github.com/paritytech/substrate/pull/13535

--- teeracle/src/lib.rs
+++ teeracle/src/lib.rs
@@ -49,7 +49,6 @@ pub mod pallet {
 	pub type OracleDataBlob<T> = BoundedVec<u8, <T as Config>::MaxOracleBlobLen>;
 
 	#[pallet::pallet]
-	#[pallet::generate_store(pub(super) trait Store)]
 	#[pallet::without_storage_info]
 	pub struct Pallet<T>(PhantomData<T>);
 
--- xcm-transactor/src/lib.rs
+++ xcm-transactor/src/lib.rs
@@ -34,7 +34,6 @@ pub mod pallet {
 	use xcm_transactor_primitives::*;
 
 	#[pallet::pallet]
-	#[pallet::generate_store(pub(super) trait Store)]
 	#[pallet::without_storage_info]
 	pub struct Pallet<T>(PhantomData<T>);
 
