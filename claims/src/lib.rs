2f813f4137ba014726e582d77d806f5d6ba95ff6
Christian Langenbacher <clangenb+gh@protonmail.ch>
2024-05-05 10:11:38 +0200

    fix test compilation

--- claims/src/lib.rs
+++ claims/src/lib.rs
@@ -636,7 +636,7 @@ mod tests {
 	// or public keys. `u64` is used as the `AccountId` and no `Signature`s are required.
 	use super::Call as ClaimsCall;
 	use frame_support::{
-		assert_err, assert_noop, assert_ok,
+		assert_err, assert_noop, assert_ok, derive_impl,
 		dispatch::{GetDispatchInfo, Pays},
 		ord_parameter_types, parameter_types,
 		traits::{ExistenceRequirement, WithdrawReasons},
