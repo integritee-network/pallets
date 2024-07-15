33d48e38929b6ae3212c4420df21dec2726f7115
Alain Brenzikofer <alain@integritee.network>
2024-07-15 16:51:46 +0200

    clippy version mismatch?

--- claims/src/lib.rs
+++ claims/src/lib.rs
@@ -423,7 +423,7 @@ pub mod pallet {
 				priority: PRIORITY,
 				requires: vec![],
 				provides: vec![("claims", signer).encode()],
-				longevity: TransactionLongevity::max_value(),
+				longevity: TransactionLongevity::MAX,
 				propagate: true,
 			})
 		}
@@ -1317,7 +1317,7 @@ mod tests {
 					priority: 100,
 					requires: vec![],
 					provides: vec![("claims", eth(&alice())).encode()],
-					longevity: TransactionLongevity::max_value(),
+					longevity: TransactionLongevity::MAX,
 					propagate: true,
 				})
 			);
@@ -1350,7 +1350,7 @@ mod tests {
 					priority: 100,
 					requires: vec![],
 					provides: vec![("claims", eth(&dave())).encode()],
-					longevity: TransactionLongevity::max_value(),
+					longevity: TransactionLongevity::MAX,
 					propagate: true,
 				})
 			);
