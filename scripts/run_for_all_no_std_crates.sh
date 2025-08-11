49e8390871584e49003621a9e1f17bf460876fbd
Christian Langenbacher <clangenb+gh@protonmail.ch>
2025-08-11 12:16:09 +0800

    remove xcm-transactor

--- scripts/run_for_all_no_std_crates.sh
+++ scripts/run_for_all_no_std_crates.sh
@@ -1,3 +1,5 @@
+export RUSTFLAGS="$RUSTFLAGS --cfg substrate_runtime"
+
 find . -name "Cargo.toml" | while read -r CARGO_TOML; do
   DIR=$(dirname "$CARGO_TOML")
   echo "Checking in directory: $DIR"
@@ -8,21 +10,11 @@ find . -name "Cargo.toml" | while read -r CARGO_TOML; do
       continue
   fi
 
-    if grep -q "runtime-benchmarks = \[" "$CARGO_TOML"; then
-        if grep -q "ksm = \[" "$CARGO_TOML"; then
-            echo "Features 'runtime-benchmarks' and 'ksm' found, adding both features."
-            cargo $COMMAND $@ --features "runtime-benchmarks ksm" --manifest-path "$CARGO_TOML"
-        else
-            echo "Feature 'runtime-benchmarks' found, adding this feature."
-            cargo $COMMAND $@ --features runtime-benchmarks --manifest-path "$CARGO_TOML"
-        fi
-    else
-        if grep -q "ksm = \[" "$CARGO_TOML"; then
-            echo "Feature 'ksm' found, adding this feature."
-            cargo $COMMAND $@ --features ksm --manifest-path "$CARGO_TOML"
-        else
-            echo "No relevant features found, running command without additional features."
-            cargo $COMMAND $@ --manifest-path "$CARGO_TOML"
-        fi
-    fi
+  if grep -q "\[features\]" "$CARGO_TOML" && grep -q "runtime-benchmarks = \[" "$CARGO_TOML"; then
+      echo "Feature 'runtime-benchmarks' found, adding this feature."
+      cargo $COMMAND $@ --features runtime-benchmarks --manifest-path "$CARGO_TOML"
+  else
+      echo "Feature 'runtime-benchmarks' not found, running command without this feature"
+      cargo $COMMAND $@ --manifest-path "$CARGO_TOML";
+  fi
 done
